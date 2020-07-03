bf9404164a4f7e0d55f5c622e13ab2330e6fef86
Gordon Beeming <gordonbeeming@outlook.com>
2020-07-03 04:30:01 +0200

    code for refresh page

---
+++ Demo-CloudflareIntegration/Models/PurgeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo_CloudflareIntegration.Models
+{
+  public class PurgeRequest
+  {
+    public PurgeRequest_File[] files { get; set; }
+  }
+
+  public class PurgeRequest_File
+  {
+    public string url { get; set; }
+  }
+}
---
+++ Demo-CloudflareIntegration/Models/PurgeResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_CloudflareIntegration.Models
+{
+  public class PurgeResponse
+  {
+    public PurgeResponse_Result result { get; set; }
+    public bool success { get; set; }
+    public object[] errors { get; set; }
+    public object[] messages { get; set; }
+  }
+
+  public class PurgeResponse_Result
+  {
+    public string id { get; set; }
+  }
+}
